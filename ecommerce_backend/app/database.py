4e3977b5d0a264106c166473270149f34789a909
Takeshy Velasquez Diaz <takeshydiaz05@gmail.com>
2026-05-03 14:24:03 -0500

    chore: actualizar apiUrl apuntando a Render

--- ecommerce_backend/app/database.py
+++ ecommerce_backend/app/database.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 # Lee la conexión de la variable de entorno (Render/Neon) o usa local por defecto
 SQLALCHEMY_DATABASE_URL = os.getenv(
